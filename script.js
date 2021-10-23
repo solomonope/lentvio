7c47bcd3d3d0dabe423444bb52593ea9292b37cc
opeyemi <solomonope@users.noreply.github.com>
2021-10-23 10:34:26 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+/*
+The mathematical formula for the calculator is as follows:
+Funding Amount required = y
+F= total Repayment amount
+I Single Flat Fee  =  ith percentage of funding Ammount
+Repayment Amount (Amount + fee) = F
+
+Therefore to determine F, we use the following formula:
+F = (y * r) + y 
+Also, i = y * r
+Example: if y = 20,000, r = 6%, 
+Then, i = 20,000 * 6% = 1,200
+Therefore , F = (20,000 * 6%) + 20,000 = 21,200
+Or (y + i) => (20,000 + 1,200) = 21,200
+
+
+
+*/
